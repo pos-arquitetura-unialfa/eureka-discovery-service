2deb6ef5397d2a763d1b8871c6d6c11937849679
Fábio Henrique Pires <fabioh.ads@gmail.com>
2020-05-09 15:11:44 -0300

    Commit Inicial

---
+++ src/test/java/br/com/travelagency/eureka/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.travelagency.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
